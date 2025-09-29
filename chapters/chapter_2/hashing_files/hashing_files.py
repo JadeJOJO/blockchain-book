f19a9b7474fe33a471297d7172e77128c743280b
Adam <zbyadam@126.com>
2025-09-29 23:54:49 +0800

    new file:   chapters/chapter_1/current_price.py 	modified:   chapters/chapter_2/hashing_files/hashing_files.py

--- chapters/chapter_2/hashing_files/hashing_files.py
+++ chapters/chapter_2/hashing_files/hashing_files.py
@@ -1,6 +1,7 @@
 from hashlib import sha256
 
-file = open("my_image.jpg", "rb")
+file = open("chapters/chapter_2/hashing_files/images/cape-town-original.jpg", "rb")
+file = open("chapters/chapter_2/hashing_files/images/cape-town-modified.jpg", "rb")
 hash = sha256(file.read()).hexdigest()
 file.close()
 
